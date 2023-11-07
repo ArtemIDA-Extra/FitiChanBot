a24c5b7dae7dccbf3cfafe9cbe28feb17123e8ab
Artem_IDA <artem-soroka-2013@ukr.net>
2023-11-07 17:44:09 +0200

    Switch to embed output in the -user command.

--- CommandsEntities.cs
+++ CommandsEntities.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
+using Microsoft.AspNetCore.Http.Features;
+using System.Runtime.Serialization.Formatters;
 
 namespace FitiChanBot
 {
@@ -15,43 +17,180 @@ namespace FitiChanBot
             var targetUser = user ?? executorUser;            // The user about whom we will display information(if there is no target user, it will display information about the command caller).
             if (executorUser.GlobalName == "Artem_IDA")       // The bot developer gets more functionality.
             {
-                string ans = "";
+                var embedAuthor = new EmbedAuthorBuilder
+                {
+                    Name = $"{targetUser.Username}",
+                    IconUrl = "https://discord.com/assets/28174a34e77bb5e5310ced9f95cb480b.png"
+                };
+
+                var embedFooter = new EmbedFooterBuilder
+                {
+                    Text = "Have a nice day! <3"
+                };
+
+                var embed = new EmbedBuilder
+                {
+                    Author = embedAuthor,
+                    Title = "User Info",
+                    Color = Color.Blue,
+                    Footer = embedFooter,
+                };
+
                 if (executorUser != targetUser)
-                    ans += "Here's what I know about this user:\n";
+                {
+                    embed.Description = "Here's what I know about this user:";
+                }
                 else
-                    ans += "Here's what I know about you :3\n";
-                ans += $"   **Username** - *{targetUser.Username}*";
+                {
+                    embed.Description = "Here's what I know about you :3";
+                }
+
+                EmbedFieldBuilder embedUsernameField;
                 if (targetUser.Discriminator != "0000")        // Checking whether the user has switched to the current naming system. If Discriminator is 0000 - means old.
-                    ans += $"#{targetUser.Discriminator}\n";
+                {
+                    embedUsernameField = new EmbedFieldBuilder
+                    {
+                        Name = "Username",
+                        Value = targetUser.Username + "#" + targetUser.Discriminator
+                    };
+                    embed.AddField(embedUsernameField);
+                }
                 else
-                    ans += "\n";
-                if (targetUser.GlobalName != null) ans += $"   **Globalname** - *{targetUser.GlobalName}*\n";
-                ans += $"   **Id** - *{targetUser.Id}*\n";
-                ans += $"   **Account create date** - *{targetUser.CreatedAt}*\n";
-                if (targetUser.GetAvatarUrl() != null) ans += $"   **Avatar Id** - *{targetUser.AvatarId}*\n";
-                if (targetUser.GetAvatarUrl() != null) ans += $"   **Avatar Url** - *[Link]({targetUser.GetAvatarUrl(Discord.ImageFormat.Auto, 512)})*\n";  // HyperLink to user avatar in Markdown markup language.
-                //ans += $"";
-                await ReplyAsync(ans);
+                {
+                    embedUsernameField = new EmbedFieldBuilder
+                    {
+                        Name = "Username",
+                        Value = targetUser.Username
+                    };
+                    embed.AddField(embedUsernameField);
+                }
+
+                EmbedFieldBuilder embedGlobalnameField;
+                if (targetUser.GlobalName != null)
+                {
+                    embedGlobalnameField = new EmbedFieldBuilder
+                    {
+                        Name = "Globalname",
+                        Value = targetUser.GlobalName
+                    };
+                    embed.AddField(embedGlobalnameField);
+                }
+
+                EmbedFieldBuilder embedIdField = new EmbedFieldBuilder
+                {
+                    Name = "ID",
+                    Value = targetUser.Id
+                };
+                embed.AddField(embedIdField);
+
+                EmbedFieldBuilder embedCreationDateField = new EmbedFieldBuilder
+                {
+                    Name = "Account create date",
+                    Value = targetUser.CreatedAt
+                };
+                embed.AddField(embedCreationDateField);
+
+                EmbedFieldBuilder embedAvatarIdField;
+                if (targetUser.GetAvatarUrl() != null)
+                {
+                    embedAvatarIdField = new EmbedFieldBuilder
+                    {
+                        Name = "Avatar ID",
+                        Value = targetUser.AvatarId
+                    };
+                    embed.AddField(embedAvatarIdField);
+                }
+
+                EmbedFieldBuilder embedAvatarUrlField;
+                if (targetUser.GetAvatarUrl() != null)
+                {
+                    embedAvatarUrlField = new EmbedFieldBuilder
+                    {
+                        Name = "Avatar Url",
+                        Value = $"[Link]({targetUser.GetAvatarUrl(Discord.ImageFormat.Auto, 512)})"    // HyperLink to user avatar in Markdown markup language.
+                    };
+                    embed.AddField(embedAvatarUrlField);
+                    embed.ImageUrl = targetUser.GetAvatarUrl();
+                }
+
+                await ReplyAsync(embed: embed.Build());
             }
             else                                              // Functionality for ordinary users.
             {
-                string ans = "";
-                if (executorUser != targetUser)
-                    ans += "Here's what I know about this user:\n";
+                var embedAuthor = new EmbedAuthorBuilder
+                {
+                    Name = $"{targetUser.Username}",
+                    IconUrl = "https://discord.com/assets/28174a34e77bb5e5310ced9f95cb480b.png"
+                };
+
+                var embedFooter = new EmbedFooterBuilder
+                {
+                    Text = "Have a nice day!"
+                };
+
+                var embed = new EmbedBuilder
+                {
+                    Author = embedAuthor,
+                    Title = "User Info",
+                    Color = Color.Blue,
+                    Footer = embedFooter,
+                    Description = "Here's what I know about this user:"
+                };
+
+                EmbedFieldBuilder embedUsernameField;
+                if (targetUser.Discriminator != "0000")      // Checking whether the user has switched to the current naming system. If Discriminator is 0000 - means old.
+                {
+                    embedUsernameField = new EmbedFieldBuilder
+                    {
+                        Name = "Username",
+                        Value = targetUser.Username + "#" + targetUser.Discriminator
+                    };
+                    embed.AddField(embedUsernameField);
+                }
                 else
-                    ans += "Here's what I know about you:\n";
-                ans += $"   **Username** - *{targetUser.Username}*";
-                if (targetUser.Discriminator != "0000")       // Checking whether the user has switched to the current naming system. If Discriminator is 0000 - means old.
-                    ans += $"#{targetUser.Discriminator}\n";
-                else
-                    ans += "\n";
-                if (targetUser.GlobalName != null) ans += $"   **Globalname** - *{targetUser.GlobalName}*\n";
-                ans += $"   **Account create date** - *{targetUser.CreatedAt}*\n";
-                if (targetUser.GetAvatarUrl() != null) ans += $"   **Avatar Url** - *[Link]({targetUser.GetAvatarUrl()})*\n";  // HyperLink to user avatar in Markdown markup language.
-                //ans += $"";
-                await ReplyAsync(ans);
+                {
+                    embedUsernameField = new EmbedFieldBuilder
+                    {
+                        Name = "Username",
+                        Value = targetUser.Username
+                    };
+                    embed.AddField(embedUsernameField);
+                }
+
+                EmbedFieldBuilder embedGlobalnameField;
+                if (targetUser.GlobalName != null)
+                {
+                    embedGlobalnameField = new EmbedFieldBuilder
+                    {
+                        Name = "Globalname",
+                        Value = targetUser.GlobalName
+                    };
+                    embed.AddField(embedGlobalnameField);
+                }
+
+                EmbedFieldBuilder embedCreationDateField = new EmbedFieldBuilder
+                {
+                    Name = "Account create date",
+                    Value = targetUser.CreatedAt
+                };
+                embed.AddField(embedCreationDateField);
+
+                EmbedFieldBuilder embedAvatarUrlField;
+                if (targetUser.GetAvatarUrl() != null)
+                {
+                    embedAvatarUrlField = new EmbedFieldBuilder
+                    {
+                        Name = "Avatar Url",
+                        Value = $"[Link]({targetUser.GetAvatarUrl(Discord.ImageFormat.Auto, 512)})"    // HyperLink to user avatar in Markdown markup language.
+                    };
+                    embed.AddField(embedAvatarUrlField);
+                    embed.ImageUrl = targetUser.GetAvatarUrl();
+                }
+
+                await ReplyAsync(embed: embed.Build());
             }
         }
+
     }
 
     [Group("dsmessage")]
