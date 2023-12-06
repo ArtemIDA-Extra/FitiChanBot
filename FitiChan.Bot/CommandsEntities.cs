295af5b8dc22c5269460f6012eaff1b598050958
Artem_IDA <artem-soroka-2013@ukr.net>
2023-12-06 22:34:42 +0200

    Modification of the output of the "Help" command. Now the name of the team is written with a note about belonging to the group and its name (if there is one).

--- FitiChan.Bot/CommandsEntities.cs
+++ FitiChan.Bot/CommandsEntities.cs
@@ -248,15 +248,19 @@ namespace FitiChanBot
             List<CommandInfo> commands = _services.GetRequiredService<CommandService>().Commands.ToList();
             
             EmbedBuilder embedBuilder = new EmbedBuilder();
+            embedBuilder
+                .WithTitle("My commands");
             foreach (CommandInfo command in commands)
             {
                 // Get the command Summary attribute information
                 string embedFieldText = command.Summary ?? "No description available\n";
-
-                embedBuilder.AddField(command.Name, embedFieldText);
+                if (command.Module.Group != null)
+                    embedBuilder.AddField($"({command.Module.Name}) {command.Name}", embedFieldText);
+                else
+                    embedBuilder.AddField(command.Name, embedFieldText);
             }
 
-            await ReplyAsync("Here's a list of commands and their description: ", false, embedBuilder.Build());
+            await ReplyAsync("", false, embedBuilder.Build());
         }
     }
 
